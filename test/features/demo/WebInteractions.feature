42d84dd100e026b6fc72099c178c60d322a515bb
Madhusuthanan-B <madhub081011@gmail.com>
2022-05-07 16:40:51 +0530

    Input box web interaction example

---
+++ test/features/demo/WebInteractions.feature
@@ -0,0 +1,13 @@
+Feature: Web Interactions
+
+    Demonstate the web interactions of various web elements
+
+    @web-interactions
+    Scenario Outline: Input Box interaction
+        Given An input box web page is opened
+        When The user selects the input box
+        Then Interact with the input box
+
+    Example:
+            | TestId    |
+            | Web_TC002 |
--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -12,13 +12,38 @@ When(/^Search with (.*)$/, async function (searchItem: string) {
     await browser.pause(1000);
 });
 
-Then(/^Click on the first search result$/, async function() {
+Then(/^Click on the first search result$/, async function () {
     const ele = await $(`//*[@id="main"]/div[2]/div/section[1]/div[1]/div[1]/a`);
     ele.click();
     await browser.pause(1000);
 });
 
-Then(/^The URL should match (.*)$/, async function(expectedURL) {
+Then(/^The URL should match (.*)$/, async function (expectedURL) {
     const browserUrl = await browser.getUrl();
     expect(browserUrl).toBe(expectedURL);
+});
+
+
+Given(/^An input box web page is opened$/, async function () {
+    await browser.url("https://the-internet.herokuapp.com/inputs");
+    await browser.setTimeout({ implicit: 15000, pageLoad: 10000 });
+    await browser.maximizeWindow();
+});
+
+
+When(/^The user selects the input box$/, async function () {
+    const ele = await $(`[type=number]`);
+    expect(ele).toBeClickable();
+    ele.click();
+});
+
+Then(/^Interact with the input box$/, async function () {
+    const numberString = `12345`;
+    const ele = await $(`[type=number]`);
+    for (let i = 0; i < numberString.length; i++) {
+        await browser.pause(1000);
+        await browser.keys(numberString[i]);
+    }
+    const finalValue = await ele.getValue();
+    expect(finalValue).toBe(numberString);
 });
